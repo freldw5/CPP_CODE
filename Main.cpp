5681975f0d3823b9c819fb9ffa9e57dabbb6362f
Farrel <asepu1358@gmail.com>
2025-03-18 05:52:31 +0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,37 @@
+#include "LoginForm.h"
+
+int main() {
+    UserSystem system("Acc.txt", "Data.txt");
+    int pilihan;
+    
+    while (true) {
+        system.clearScreen();
+        cout << "===== SISTEM KONVERSI RUPIAH =====\n";
+        cout << "1. Register\n";
+        cout << "2. Login\n";
+        cout << "3. Keluar\n";
+        cout << "Pilihan anda: ";
+        cin >> pilihan;
+        
+        switch (pilihan) {
+            case 1:
+                system.registerUser();
+                break;
+            case 2:
+                if (system.login()) {
+                    system.mainMenu();
+                }
+                break;
+            case 3:
+                cout << "\nTerima kasih telah menggunakan program ini!\n";
+                return 0;
+            default:
+                cout << "\nPilihan tidak valid. Silakan coba lagi.\n";
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cin.get();
+                break;
+        }
+    }
+    
+    return 0;
+}
